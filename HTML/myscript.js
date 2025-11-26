12caafa108c76eb8f7310a7a37171bbd7ea31508
sahanaangadi2004 <sahana.angadi2004@gmail.com>
2025-11-26 11:02:00 +0530

    commit

---
+++ HTML/myscript.js
@@ -0,0 +1,26 @@
+var dayno=5
+if(dayno==0)
+{
+    console.log("sunday")
+}
+else if(dayno==1)
+{
+    console.log("monday")
+}
+else if(dayno==2)
+{
+    console.log("tuesday")
+}
+else if(dayno==3)
+{
+    console.log("wednesday")
+}
+else if(dayno==4)
+
+{
+    console.log("thursday")
+}
+else if(dayno==5)
+{
+    console.log("friday")
+}
