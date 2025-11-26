faf300275319206e6a37e840672efc31a7a1f1cf
sahanaangadi2004 <sahana.angadi2004@gmail.com>
2025-11-26 16:57:08 +0530

    commit

--- HTML/myscript.js
+++ HTML/myscript.js
@@ -1,34 +1,80 @@
-var dayno=0
-if(dayno==0)
-{
-    document.writeln("sunday")
-}
-else if(dayno==1)
-{
-    alert("monday")
-}
-else if(dayno==2)
-{
-    console.log("tuesday")
-}
-else if(dayno==3)
-{
-    console.log("wednesday")
-}
-else if(dayno==4)
+// var dayno=0
+// if(dayno==0)
+// {
+//     document.writeln("sunday")
+// }
+// else if(dayno==1)
+// {
+//     alert("monday")
+// }
+// else if(dayno==2)
+// {
+//     console.log("tuesday")
+// }
+// else if(dayno==3)
+// {
+//     console.log("wednesday")
+// }
+// else if(dayno==4)
+// {
+//     console.log("thursday")
+// }
+// else if(dayno==5)
+// {
+//     console.log("friday")
+// }
+// else if(dayno==6)
+// {
+//     console.log("saturday")
+// }
+// else (dayno==7)
+// {
+//     console.log("default")
+// }
 
+
+
+// var dayno=3
+// switch(dayno)
+// {
+//     case 1:console.log("monday")
+//     break;
+//     case 2:console.log("tuesday")
+//     break;
+//     case 3:console.log("wednesday")
+//     break;
+//     case 4:console.log("thursday")
+//     break;
+//     case 5:console.log("friday")
+//     break;
+//     case 6:console.log("saturday")
+//     break;
+//     case 7:console.log("sunday")
+//     break;
+//     default:console.log("default")
+// }
+
+
+
+// var x=5
+// console.log(x%2==0?"even":"odd")
+
+
+
+var char=prompt("enter a character:");
+switch(char)
 {
-    console.log("thursday")
-}
-else if(dayno==5)
-{
-    console.log("friday")
-}
-else if(dayno==6)
-{
-    console.log("saturday")
-}
-else (dayno==7)
-{
-    console.log("default")
+    case 'A':console.log("apple")
+    break;
+    case 'B':console.log("ball")
+    break;
+    case 'C':console.log("cat")
+    break;
+    case 'D':console.log("dog")
+    break;
+    case 'E':console.log("elephant")
+    break;
+    case '?':console.log("mango")
+    break;
+    default:console.log("enter a valid character");
 }
